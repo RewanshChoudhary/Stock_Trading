4d76b054b0282766449f3f7998ac0a6154814d44
RewanshChoudhary <rewanshchoudhary57@gmail.com>
2025-11-01 18:05:21 +0530

    0.1%

---
+++ src/main/java/com/example/StockTrading/model/PriceHistory.java
@@ -0,0 +1,25 @@
+package com.trading.app.model;
+
+import com.example.StockTrading.model.Stock;
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "price_history")
+@Data
+public class PriceHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "stock_id", nullable = false)
+    private Stock stock;
+
+    @Column(nullable = false)
+    private Double price;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
---
+++ src/main/java/com/example/StockTrading/model/User.java
@@ -0,0 +1,32 @@
+package com.example.StockTrading.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name="users")
+
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String passwordHash; // store hashed password only
+
+    @Column(nullable = false)
+    private Double balance = 0.0;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+}
